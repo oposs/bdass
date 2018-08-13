c8f19bc7999adf184c6207a914148662987b5adc
Tobias Oetiker <tobi@oetiker.ch>
2018-08-13 17:03:49 +0200

    lighten base class

--- lib/Bdass/ConnectionPlugin/Base.pm
+++ lib/Bdass/ConnectionPlugin/Base.pm
@@ -25,106 +25,38 @@ has type => sub {
     die "plugin must specify its type";
 };
 
-sub makeTransport {
+has tokenFile => 'BDASS_TOKEN.txt';
+
+=head2 target
+
+which machine are we talking to ?
+
+=cut
+
+has 'target';
+
+=head2 listFolders
+
+return a list of folders which could be archived
+
+=cut
+
+sub listFolders {
     my $self = shift;
-    my $dest = shift;
-    my $ssh = Mojo::IOLoop::ReadWriteFork->new;
-
-    # Emitted if something terrible happens
-    $ssh->on(error => sub { 
-        my ($fork, $error) = @_; 
-        $self->log->error($error); 
-    });
-
-    # Emitted when the child completes
-    $ssh->on(close => sub { 
-        my ($fork, $exit_value, $signal) = @_;
-    });
-    # Start the application
-    $ssh->start({
-        program => "ssh",
-        program_args => [$dest],
-        conduit => 'pty',
-        raw => 1,
-        env => {}
-    });
-    $self->setPrompt();
-    return $ssh;
+    return Mojo::Promise->new;
 };
 
-has log => sub { shift->app->log };
+=head2 streamFolder
 
-sub run {
-    my $self   = shift;
-    local @ARGV = @_ if @_;
-    GetOptions(\%opt,
-            'verbose|v','cfgglob=s');
-    
-    my ($flavor,$dest) = @ARGV;
+convert the content of the folder to a configurable archive format
+return an eventemitter with 'read' 'error' 'close' events
 
-    if ($flavor ne 'ubuntu'){
-        die "Sorry $flavor is not supported yet\n";
-    }
-    $self->destHost($dest);  
-    $self->setPrompt('hello> ')->then(sub{
-        $self->checkTransparency();
-    })->wait;
-    Mojo::IOLoop->start;
-}
+=cut
 
-sub setPrompt {
+sub {
     my $self = shift;
-    my $promise = Mojo::Promise->new;
-    my $prompt = shift;
-    my $ssh = $self->ssh;
-    my $buff = '';
-    my $okCb;
-    $okCb = sub {
-        my ($fork, $buf) = @_;
-        $buff .= $buf;
-        warn dumper($buff);
-        if ($buff =~ /$prompt/){
-            $fork->unsubscribe(read=>$okCb);
-            $self->log->info("prompt check successful");
-            $promise->resolve();
-        }
-    };    
-    $ssh->on(read=>$okCb);
-    $ssh->write(qq{stty -echo\nPS1='${prompt}'\n});
-    return $promise;
-}
+    return Mojo::EventEmitter->new;
+};
 
-sub checkTransparency {;
-    my $self = shift;
-    my $promise = Mojo::Promise->new;
-    my $send_ord = 0;
-    my $buff = ''  ;
-    my $ssh = $self->ssh;
-    my $readCb;
-    $readCb = sub {
-        my ($fork, $buf) = @_;
-        $buff .= $buf;
-        #warn dumper($buff);
-        # $self->log->debug("got >".dumper($buf));
-        while ($buff =~ s/^.*?>(.)<\n//s){
-            my $ord = ord($1);
-            if ($ord != $send_ord){
-                $self->log->error("expected chr($send_ord) got chr($ord)");
-            }
-            if ($ord == 255){
-                $fork->unsubscribe(read=>$readCb);
-                $self->log->info("com transparency check successful");
-                $promise->resolve();
-            }
-            else {
-                $fork->write('>'.chr(++$send_ord).'<'."\n");
-            }
-        }
-    };
-    $ssh->on(read => $readCb);
-    $ssh->write("dd bs=4 count=256\n");
-    $ssh->write('>'.chr($send_ord)."<\n");
-    return $promise;
-}
-
+sub 
 1;
